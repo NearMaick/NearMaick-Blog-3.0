c1df24d76b817e7c2fa7de9ef15b651f1d260378
Maick Souza <maick_a_s@msn.com>
2021-12-01 15:06:00 -0300

    feat(web):navbar #1

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,12 +1,46 @@
 import Link from 'next/link'
+import {
+  FaAddressBook,
+  FaBlog,
+  FaBriefcase,
+  FaHome,
+  FaUser,
+} from 'react-icons/fa'
 
 import styles from './styles.module.scss'
 
 export function Navbar() {
   return (
-    <nav className={styles.navContainer}>
+    <nav className={styles.navbarContainer}>
       <Link href="/">
-        <a>Navbar</a>
+        <a>
+          <FaHome />
+          Home
+        </a>
+      </Link>
+      <Link href="/">
+        <a>
+          <FaUser />
+          Sobre
+        </a>
+      </Link>
+      <Link href="/">
+        <a>
+          <FaBriefcase />
+          Portfólio
+        </a>
+      </Link>
+      <Link href="/">
+        <a>
+          <FaBlog />
+          Blog
+        </a>
+      </Link>
+      <Link href="/">
+        <a>
+          <FaAddressBook />
+          Contato
+        </a>
       </Link>
     </nav>
   )
