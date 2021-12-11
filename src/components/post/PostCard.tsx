65625fa0e8173a8fc901fe96c52038f7d8a64c52
Maick Souza <maick_a_s@msn.com>
2021-12-11 13:52:41 -0300

    refactor(web):blog postcard #1

---
+++ src/components/post/PostCard.tsx
@@ -0,0 +1,27 @@
+import { FaCalendar, FaLink, FaUser } from 'react-icons/fa'
+
+import styles from '../../styles/components/post/post-card.styles.module.scss'
+import { LinkButton } from '../LinkButton'
+export function PostCard() {
+  return (
+    <div className={styles.box}>
+      <div className={styles.image}>
+        <img src="/images/blog1.jpg" alt="blog" />
+      </div>
+      <div className={styles.content}>
+        <div>
+          <div>
+            <FaCalendar /> 21 de dez de 2021
+            <FaUser /> Maick Souza
+          </div>
+        </div>
+        <h3>Título do blog</h3>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi,
+          illum.
+        </p>
+      </div>
+      <LinkButton link="/post/batata" icon={FaLink} description="leia mais" />
+    </div>
+  )
+}
--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -1,116 +1,22 @@
-import Link from 'next/link'
 import { FaCalendar, FaLink, FaUser } from 'react-icons/fa'
 
+import { Heading } from '../components/Heading'
+import { PostCard } from '../components/post/PostCard'
 import styles from '../styles/pages/Blog.module.scss'
 
 export default function Blog() {
   return (
     <>
-      <div>
-        <h1 className={styles.heading}>
-          nearmaick <span>blog</span>
-        </h1>
-      </div>
+      <Heading title="nearmaick" subtitle="blog" />
 
       <div className={styles.blogContainer}>
-        <div className={styles.box}>
-          <div className={styles.image}>
-            <img src="/images/blog1.jpg" alt="blog" />
-          </div>
-          <div className={styles.content}>
-            <div>
-              <div>
-                <FaCalendar /> 21 de dez de 2021
-                <FaUser /> Maick Souza
-              </div>
-            </div>
-            <h3>Título do blog</h3>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi,
-              illum.
-            </p>
-          </div>
-          <Link href="/post/batata">
-            <a className={styles.linkButton} href="#">
-              <div>
-                <FaLink />
-                Leia Mais...
-              </div>
-            </a>
-          </Link>
-        </div>
-        <div className={styles.box}>
-          <div className={styles.image}>
-            <img src="/images/blog1.jpg" alt="blog" />
-          </div>
-          <div className={styles.content}>
-            <div>
-              <div>
-                <FaCalendar /> 21 de dez de 2021
-                <FaUser /> Maick Souza
-              </div>
-            </div>
-            <h3>Título do blog</h3>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi,
-              illum.
-            </p>
-          </div>
-          <a className={styles.linkButton} href="#">
-            <div>
-              <FaLink />
-              Leia Mais...
-            </div>
-          </a>
-        </div>
-        <div className={styles.box}>
-          <div className={styles.image}>
-            <img src="/images/blog1.jpg" alt="blog" />
-          </div>
-          <div className={styles.content}>
-            <div>
-              <div>
-                <FaCalendar /> 21 de dez de 2021
-                <FaUser /> Maick Souza
-              </div>
-            </div>
-            <h3>Título do blog</h3>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi,
-              illum.
-            </p>
-          </div>
-          <a className={styles.linkButton} href="#">
-            <div>
-              <FaLink />
-              Leia Mais...
-            </div>
-          </a>
-        </div>
-        <div className={styles.box}>
-          <div className={styles.image}>
-            <img src="/images/blog1.jpg" alt="blog" />
-          </div>
-          <div className={styles.content}>
-            <div>
-              <div>
-                <FaCalendar /> 21 de dez de 2021
-                <FaUser /> Maick Souza
-              </div>
-            </div>
-            <h3>Título do blog</h3>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi,
-              illum.
-            </p>
-          </div>
-          <a className={styles.linkButton} href="#">
-            <div>
-              <FaLink />
-              Leia Mais...
-            </div>
-          </a>
-        </div>
+        <PostCard />
+        <PostCard />
+        <PostCard />
+        <PostCard />
+        <PostCard />
+        <PostCard />
+        <PostCard />
       </div>
     </>
   )
